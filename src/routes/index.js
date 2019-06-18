0f763f154dd71e3317e45994265c1520fa6715f4
danielmateoarevaloangel@gmail.com <danielmateoarevaloangel@gmail.com>
2019-06-18 15:44:24 -0500

    correcion

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,10 @@ const path = require('path');
 var request = require('request'); // "Request" library
 var token="";
 var Spotify = require('node-spotify-api');
+var album='';
 
 router.post('/busqueda',async(req,res)=>{
-
+album= req.body.album;
 var client_id = '6ce286bac2d5477589dbf405a021ecfa'; // Your client id
 var client_secret = '9d9a066e93214729aba53c5bc0217d69'; // Your secret
 
@@ -23,6 +24,7 @@ var authOptions = {
 };
 
 request.post(authOptions, function(error, response, body) {
+
     if (!error && response.statusCode === 200) {
   
     console.log(body)
@@ -31,35 +33,16 @@ request.post(authOptions, function(error, response, body) {
     console.log(token)
     
   });
-/*
-var authOptions = {
-  url: 'https://api.spotify.com/v1/search?q=tania*&type=artist',
-  headers: {
-    'Authorization': token
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
-Request.get(authOptions, (error, response, body) => {
-    if(error) {
-        console.log(error)
-    }
-    console.log(JSON.parse(body));
-    console.log("aquiiii");
-});
-*/
 var spotify = new Spotify({
     id : '6ce286bac2d5477589dbf405a021ecfa', 
     secret : '9d9a066e93214729aba53c5bc0217d69' 
   });
-   
-  spotify.search({ type: 'album', query: 'All the Small Things' }, function(err, data) {
+ 
+  spotify.search({ type: 'album', query: album}, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-   
+
   console.log(data); 
   res.json(data)
   });
