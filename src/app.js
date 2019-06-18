6ee0c369455ed02b82ad9beb21a4f3cc584bbf61
danielmateoarevaloangel@gmail.com <danielmateoarevaloangel@gmail.com>
2019-06-18 13:37:56 -0500

    token

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
-var Request = require("request");
+var request = require('request'); // "Request" library
 
-Request.get("https://developer.spotify.com", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
+var client_id = '6ce286bac2d5477589dbf405a021ecfa'; // Your client id
+var client_secret = '9d9a066e93214729aba53c5bc0217d69'; // Your secret
+
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+  
+    console.log(body)
     }
-    console.dir(JSON.parse(body));
-});
+  });
+
+    
