40ae3b6727857da8a98200a28a5a9547f3595c7a
danielmateoarevaloangel@gmail.com <danielmateoarevaloangel@gmail.com>
2019-06-18 14:48:19 -0500

    busqueda

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 var request = require('request'); // "Request" library
-const token;
+var token="";
 router.post('/busqueda',async(req,res)=>{
 
 var client_id = '6ce286bac2d5477589dbf405a021ecfa'; // Your client id
@@ -25,8 +25,29 @@ request.post(authOptions, function(error, response, body) {
   
     console.log(body)
     }
-    token = body.toke.access_token
+    token = body.access_token
     console.log(token)
   });
+
+var authOptions = {
+  url: 'https://api.spotify.com/v1/search?q=tania*&type=artist',
+  headers: {
+    'Authorization': 'Bearer' + (new Buffer(token).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+  Request.get("",Authorization=token, (error, response, body) => {
+    if(error) {
+        
+    }
+    console.log(JSON.parse(body));
+    console.log("aquiiii");
+});
+
+  res.json({token:token})
 })
+
 module.exports=router;
